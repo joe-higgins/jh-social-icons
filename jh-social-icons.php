d000f24892013bf6e6f0ebb57c2f88fba807e185
Joe Higgins <joehiggins.bass@gmail.com>
2019-04-08 13:22:36 -0400

    Initial commit

---
+++ jh-social-icons.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: jh-social-icons
+* Plugin URI: www.joehiggins.me
+* Plugin to add font awesome social media icons through customizer
+* Version: 1.0
+* Author: Joe Higgins
+* License: GPL2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+ ?>
